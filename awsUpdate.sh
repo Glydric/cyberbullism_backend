c49260ceb515760a6b6837179ddaad9fb961532f
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-06 14:18:49 +0200

    added a command to push folder updates to aws

---
+++ awsUpdate.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+scp -i ~/Downloads/key.pem /Users/leonardomigliorelli/Desktop/Unicam/Anno\ 3\ 2022-2023/Modulo\ Project/php\ sql\ files/* ubuntu@ec2-18-102-69-85.eu-south-1.compute.amazonaws.com:~/ 
