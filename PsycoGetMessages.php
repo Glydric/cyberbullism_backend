7d4e72e04df40ba96b94d5f54d83800b9411517b
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-10 09:36:33 +0200

    used function

--- PsycoGetMessages.php
+++ PsycoGetMessages.php
@@ -3,13 +3,14 @@ require('config.php');
 // get user
 $email = removeSQLDelimitersFrom($_POST['email']);
 $password = removeSQLDelimitersFrom($_POST['password']);
+$otherEmail = removeSQLDelimitersFrom($_POST['otherEmail']);
 
 checkExists("psyco", $conn, $email, $password);
 
-
-// get chats
-$otherEmail = removeSQLDelimitersFrom($_POST['otherEmail']);
-$psyco_query = "SELECT
+function getChats($conn, $email, $otherEmail)
+{
+  // get chats
+  $psyco_query = "SELECT
     user_email AS otherEmail,
     nome,
     cognome,
@@ -17,26 +18,27 @@ $psyco_query = "SELECT
     data,
     gravita,
     CASE WHEN sender = 0 THEN 1 WHEN sender = 1 THEN 0
-END AS sender
-FROM
-    messaggio join utente on user_email=email
-WHERE
-    psyco_email = '$email' AND user_email = '$otherEmail'
-ORDER BY DATA
-DESC
-";
-$result = mysqli_query($conn, $psyco_query);
+  END AS sender
+  FROM
+      messaggio join utente on user_email=email
+  WHERE
+      psyco_email = '$email' AND user_email = '$otherEmail'
+  ORDER BY DATA
+  DESC
+  ";
+  $result = mysqli_query($conn, $psyco_query);
 
-if (!$result) {
-  die(mysqli_error($conn));
-}
+  if (!$result) {
+    die(mysqli_error($conn));
+  }
 
-$rows = array();
-while ($row = mysqli_fetch_assoc($result)) {
-  $rows[] = $row;
-}
+  $rows = array();
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
 
-echo json_encode($rows);
+  echo json_encode($rows);
 
-mysqli_free_result($result);
+  mysqli_free_result($result);
+};
 mysqli_close($conn);
