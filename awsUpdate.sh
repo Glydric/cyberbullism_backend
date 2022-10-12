00cd1182b82b1e64284fee37e946ca26fe253ea1
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-12 16:35:32 +0200

    update

--- awsUpdate.sh
+++ awsUpdate.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp -i ~/Downloads/key.pem /Users/leonardomigliorelli/Desktop/Unicam/Anno\ 3\ 2022-2023/Modulo\ Project/php\ sql\ files/*.php ubuntu@ec2-15-160-141-119.eu-south-1.compute.amazonaws.com:/var/www/html 
+scp -i ~/Downloads/key.pem /Users/leonardomigliorelli/Desktop/Unicam/Anno\ 3\ 2022-2023/Modulo\ Project/php\ sql\ files/*.php ubuntu@15.160.153.143:/var/www/html 
