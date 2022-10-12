2bf8d8958af8a5866baada0c0a6c78d0d9b2bad8
leomiglio02 <56474420+Glydric22@users.noreply.github.com>
2022-10-12 16:35:39 +0200

    added ws

--- PsycoGetWSMessages.php
+++ PsycoGetWSMessages.php
@@ -1,4 +1,8 @@
 <?php
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
 require('config.php');
 // get user
 $email = removeSQLDelimitersFrom($_POST['email']);
@@ -41,5 +45,39 @@ function getChats($conn, $email, $otherEmail)
 
     mysqli_free_result($result);
 };
+
+class Socket implements MessageComponentInterface {
+
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+
+        // Store the new connection in $this->clients
+        $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+
+        foreach ( $this->clients as $client ) {
+
+            if ( $from->resourceId == $client->resourceId ) {
+                continue;
+            }
+
+            $client->send( "Client $from->resourceId said $msg" );
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
 
 getChats($conn, $email, $otherEmail);
