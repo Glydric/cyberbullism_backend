6b009fee5b75dc5658f9638ebe7a50ee1a70536b
Leonardo Migliorelli <leo.miglio@outlook.com>
2022-10-17 07:49:46 +0000

    using part of old php

--- WS/psycoWebSocket.py
+++ WS/psycoWebSocket.py
@@ -1,40 +1,12 @@
-import mysql.connector
+import requests
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.websocket as WS
 
-connection = mysql.connector.connect(
-    host="localhost",
-    database="cyberbullism",
-    user="cyber",
-    password="AdminDB02",
-)
 
-query = """
-SELECT
-    user_email AS otherEmail,
-    nome,
-    cognome,
-    testo,
-    data,
-    gravita,
-    CASE WHEN send_by_user = 0 THEN 1 WHEN send_by_user = 1 THEN 0
-END AS send_by_user
-FROM
-    messaggio join utente on user_email=email
-WHERE
-    psyco_email = '$email' AND user_email = '$otherEmail'
-ORDER BY DATA
-DESC
-"""
-
-def getSql():
-    cursor = connection.cursor()
-    cursor.execute(query)
-
-    for x in cursor.fetchall():
-        print(x)
+def getSql(email: str, password: str, otherEmail: str):
+    pass
 
 
 
@@ -50,18 +22,14 @@ class WebSocket(WS.WebSocketHandler):
         # metodo eseguito alla ricezione di un messaggio
         # la stringa 'message' rappresenta il messaggio
 
-        # print(getHeader(self, "email"))
+        print(getHeader(self, "email"))
         self.write_message(r"Messaggio ricevuto: {message}")
         print("Messaggio ricevuto: %s" % message)
 
     def on_close(self):
         # metodo eseguito alla chiusura della connessione
-        # self.loop.stop()
+        self.loop.stop()
         print("Connessione chiusa")
-
-    # def check_origin(self, origin):
-    #     return True
-
 
 def make_app():
     return tornado.web.Application(
