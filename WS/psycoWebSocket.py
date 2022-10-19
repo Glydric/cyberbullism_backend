a65191f32ec0eb8ebdc6a12099032083aa2e1ca9
Leonardo Migliorelli <leo.miglio@outlook.com>
2022-10-19 10:42:27 +0000

    functions into class

--- WS/psycoWebSocket.py
+++ WS/psycoWebSocket.py
@@ -1,3 +1,4 @@
+from unittest import result
 import requests
 import tornado.httpserver
 import tornado.ioloop
@@ -6,35 +7,57 @@ import tornado.websocket as WS
 
 DbServer = "http://leonardomigliorelli.altervista.org"
 
-def getSql(email: str, password: str, otherEmail: str) -> str:
-    arguments = {
-        "email": email,
-        "password": password,
-        "otherEmail": otherEmail,
-    }
-
-    return requests.post(DbServer+"/PsycoGetMessages.php", json=arguments).text
-
 
 class WebSocket(WS.WebSocketHandler):
+    email: str
+    password: str
+    otherEmail: str
+
+    def getMessages(self) -> str:
+        arguments = {
+            "email": self.email,
+            "password": self.password,
+            "otherEmail": self.otherEmail,
+        }
+
+        result = requests.post(
+            DbServer + "/PsycoGetMessages.php",
+            data=arguments,
+        )
+        return result.text
+
+    def sendMessage() -> str:
+        pass
+
     def getHeader(self, name: str):
         return self.request.headers.get(name)
 
     def open(self):
         # metodo eseguito all'apertura della connessione
-        print("Nuova connessione")
+
+        self.email = self.getHeader("email")
+        self.password = self.getHeader("password")
+        self.otherEmail = self.getHeader("otherEmail")
+
+        print(
+            "Nuova connessione instanziata con i seguenti valori\n"
+            + f"{self.email}, {self.password}, {self.otherEmail}"
+        )
 
     def on_message(self, message):
         # metodo eseguito alla ricezione di un messaggio
         # la stringa 'message' rappresenta il messaggio
 
-        response = getSql(
-            self.getHeader("email"),
-            self.getHeader("password"),
-            self.getHeader("psyco_email"),
-        )
-
         print(f"Messaggio ricevuto: {message}")
+
+        if message == "reload":
+
+            response = self.getMessages()
+        elif message == "send":
+            response = self.sendMessage()
+        else:
+            response = "Request Error"
+
         self.write_message(f"Risposta: {response}")
 
     def on_close(self):
