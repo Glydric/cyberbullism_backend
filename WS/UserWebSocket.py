0b8bf1c30d9f1f2ff9f18cb2b548323427d4973b
Leonardo Migliorelli <leo.miglio@outlook.com>
2022-10-20 09:25:02 +0000

    various changes and better

--- WS/UserWebSocket.py
+++ WS/UserWebSocket.py
@@ -1,65 +1,65 @@
-import requests
+import requests, re
 import tornado.websocket as WS
 
 
-DbServer = "http://leonardomigliorelli.altervista.org"
-
-
 class UserWebSocket(WS.WebSocketHandler):
-    email: str
-    password: str
-    otherEmail: str
+    DbServer = "http://leonardomigliorelli.altervista.org"
+    arguments: dict
 
-    def switch(self, message: str) -> str:
-        if message == "reload":
-            return self.getMessages()
-        elif message == "send":
-            return self.sendMessage()
-        else:
-            return "Request Error"
-
-    def getMessages(self) -> str:
-        arguments = {
+    # metodo eseguito all'apertura della connessione
+    def open(self):
+        self.arguments = {
             "email": self.email,
             "password": self.password,
             "otherEmail": self.otherEmail,
         }
 
+        print(
+            "Nuova connessione instanziata con i seguenti valori\n"
+            + f"{self.arguments['email']}, {self.arguments['password']}, {self.arguments['otherEmail']}"
+        )
+
+    # esegue uno switch in base al messaggio ricevuto ed esegue i relativi metodi
+    def switch(self, message: str) -> str:
+        if message == "reload":
+            return self.getMessages()
+        elif message.find("send") != -1:
+            # remove first send and successives spaces
+            m = re.sub("send +", "", message, 1)
+            # send message
+            return self.sendMessage(m)
+        return "Request Error"
+
+    # Consente di ottenere i messaggi
+    def getMessages(self) -> str:
         result = requests.post(
-            DbServer + "/UserGetMessages.php",
-            data=arguments,
+            self.DbServer + "/UserGetMessages.php",
+            data=self.arguments,
         )
         return result.text
 
-    def sendMessage() -> str:
-        pass
+    # Consente di inviare i messaggi
+    def sendMessage(self, message: dict) -> str:
+        self.arguments["message"] = message
+        result = requests.post(
+            self.DbServer + "/UserGetMessages.php",
+            data=self.arguments,
+        )
+        self.arguments.pop("message")
+        return result.text
 
+    # Metodo di comodo che ci permette di ottenere i valori post passati
     def getHeader(self, name: str):
         return self.request.headers.get(name)
 
-    def open(self):
-        # metodo eseguito all'apertura della connessione
 
-        self.email = self.getHeader("email")
-        self.password = self.getHeader("password")
-        self.otherEmail = self.getHeader("otherEmail")
-
-        print(
-            "Nuova connessione instanziata con i seguenti valori\n"
-            + f"{self.email}, {self.password}, {self.otherEmail}"
-        )
-
+    # metodo eseguito alla ricezione di un messaggio
     def on_message(self, message):
-        # metodo eseguito alla ricezione di un messaggio
-        # la stringa 'message' rappresenta il messaggio
-
         print(f"Messaggio ricevuto: {message}")
 
-        response = self.switch(message)
+        self.write_message(f"Risposta: {self.switch(message)}")
 
-        self.write_message(f"Risposta: {response}")
-
+    # metodo eseguito alla chiusura della connessione
     def on_close(self):
-        # metodo eseguito alla chiusura della connessione
         # tornado.ioloop.IOLoop.instance().stop()
         print("Connessione chiusa")
