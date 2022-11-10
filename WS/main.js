9ccf6338a920640b7753628dcacaf46852fc746d
leomiglio02 <leo@miglio.dev>
2022-11-10 20:47:31 +0100

    differentiating requests based on path

--- WS/main.js
+++ WS/main.js
@@ -1,25 +1,65 @@
 const WebSocketServer = require("ws")//.Server
-// const http = require('http');
+const url = require("url")
+const unirest = require("unirest");
 
-const user = require("./userWS")
-const psyco = require("./psycoWS")
-
-// var webServer = http.createServer();
-// webServer.listen(80);
-
-
-const psycoServer = new WebSocketServer.Server({
-    port: 80,
-    // server: webServer,
-    path: "/psyco"
+const server = new WebSocketServer.Server({
+    port: 80
 });
 
-const userServer = new WebSocketServer.Server({
-    port: 81,
-    // server: webServer,
-    path: "/user"
-});
+const dbUrl = 'http://leonardomigliorelli.altervista.org'
 
-// userServer.on('request', user.userServerConnection)
-userServer.on('connection', user.userServerConnection)
-psycoServer.on('connection', psyco.psycoServerConnection)
+var jsonAuth
+
+function setAuth(message) {
+    console.log("set ricevuto")
+    jsonAuth = JSON.parse(message)
+}
+
+function serverConnection(conn, req) {
+    conn.id
+    const path = url.parse(req.url, true).path;
+
+    if (path != "/Psyco" && path != "/User")
+        conn.close()
+
+    console.log(`new connection in ${path}`)
+
+    function sendMessage(message) {
+        console.log("send ricevuto")
+        jsonAuth["testo"] = message
+
+        unirest.post(dbUrl + path + "SendMessage.php")
+            .send(jsonAuth)
+            .then(res => console.log(res.status))
+    }
+
+    function reload() {
+        unirest.post(dbUrl + path + "GetMessages.php")
+            .send(jsonAuth)
+            .then(
+                res => conn.send(
+                    res.status == 200
+                        ? res.body
+                        : "WebSocket Connection Error"
+                )
+            )
+    }
+
+    conn.onmessage = msg => {
+        const message = `${msg.data}`
+
+        if (message.startsWith("set"))
+            setAuth(message.replace("set ", ""))
+
+        if (message.startsWith("send"))
+            sendMessage(message.replace("send ", ""))
+
+        reload()
+    }
+
+    conn.onclose = () => console.log(`Client disconnected`)
+
+    conn.onerror = () => console.log("An error occurred")
+}
+
+server.on('connection', serverConnection)
