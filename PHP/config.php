b38c02bf68383bf2576e6ab039b3b4639baa7d99
leomiglio02 <leo@miglio.dev>
2022-11-21 13:43:47 +0100

    add config

---
+++ PHP/config.php
@@ -0,0 +1,31 @@
+<?php
+$host = "localhost";
+$database = "cyberbullism";
+$username = "cyber"; #"admin";
+$password = "";
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (mysqli_connect_errno())
+  die("Connection failed: " . mysqli_connect_error());
+
+// Function to remove the spacial
+function removeSQLDelimitersFrom($string)
+{
+  // Viene usato preg_replace() per rimuovere i caratteri
+  return preg_replace('/[\'";]+/s', ' ', $string);
+};
+function checkExists($tab, $conn, $email, $password)
+{
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    die("invalid-email");
+
+  $query = "select * from $tab where email='$email' and password='$password'";
+  $result = mysqli_query($conn, $query);
+  if (!$result) {
+    echo (mysqli_error($conn));
+  }
+  if (mysqli_num_rows($result) == 0) {
+    die("user-not-found");
+  }
+  mysqli_free_result($result);
+}
