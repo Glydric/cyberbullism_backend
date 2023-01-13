900657f3a7b09d2ea99eefddebc14aea6f7d592f
Glydric <leo@miglio.dev>
2023-01-13 11:18:47 +0100

    resolved issue

--- PHP/PsycoPresaInCarica.php
+++ PHP/PsycoPresaInCarica.php
@@ -7,13 +7,7 @@ $password = removeSQLDelimitersFrom($_POST['password']);
 if (!filter_var($email, FILTER_VALIDATE_EMAIL))
     die("invalid-email");
 
-$result = mysqli_query($conn, "select * from psyco where email='$email' and password='$password'");
-if (!$result) {
-    echo (mysqli_error($conn));
-}
-if (mysqli_num_rows($result) == 0) {
-    die("user-not-found");
-}
+checkExists("psyco", $conn, $email, $password);
 
 // send message
 $otherEmail = removeSQLDelimitersFrom($_POST['otherEmail']);
